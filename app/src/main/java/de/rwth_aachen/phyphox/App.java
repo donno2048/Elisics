1218e62993513093a4b30717fb38d96787550dcf
donno2048 <just4now666666@gmail.com>
2020-11-08 13:52:09 +0200

    Files removed

--- app/src/main/java/de/rwth_aachen/phyphox/App.java
+++
@@ -1,9 +0,0 @@
-package de.rwth_aachen.phyphox;
-
-import android.app.Application;
-
-//This extension to application is only used to store measured data in memory as this may easily exceed the amount of data allowed on the transaction stack
-
-public class App extends Application {
-    public PhyphoxExperiment experiment = null;
-}
