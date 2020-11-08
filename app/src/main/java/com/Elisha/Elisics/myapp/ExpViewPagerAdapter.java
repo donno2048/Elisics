25eda7410f0e73c39227cffa696c72d4e7f60e16
donno2048 <just4now666666@gmail.com>
2020-11-08 13:51:22 +0200

    Files added

---
+++ app/src/main/java/com/Elisha/Elisics/myapp/ExpViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.Elisha.Elisics.myapp;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ExpViewPagerAdapter extends FragmentPagerAdapter {
+
+    private PhyphoxExperiment experiment;
+
+    public ExpViewPagerAdapter(FragmentManager fm, PhyphoxExperiment experiment) {
+        super(fm);
+        this.experiment = experiment;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return ExpViewFragment.newInstance(position);
+    }
+
+    @Override
+    public int getCount() {
+        if (experiment != null)
+            return experiment.experimentViews.size();
+        else
+            return 0;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return experiment.experimentViews.get(position).name;
+    }
+
+}
---
+++ app/src/main/java/com/Elisha/Elisics/myapp/SensorInputTimeReference.java
@@ -0,0 +1,23 @@
+package com.Elisha.Elisics.myapp;
+
+public class SensorInputTimeReference {
+    private long t0 = 0;
+    private boolean valid = true;
+
+    public void set(long t0) {
+        this.t0 = t0;
+        valid = true;
+    }
+
+    public void reset() {
+        valid = false;
+    }
+
+    public long get() {
+        return t0;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+}
